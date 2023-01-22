2e5b9edfed466a1edd1329bf864566186f80a254
skippharaoh <caniggiathompson@gmail.com>
2023-01-22 10:48:26 -0500

    Blog Preview mode setup complete

---
+++ components/BlogList.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  posts: Post[];
+};
+
+export default function BlogList({ posts }: Props) {
+  console.log(posts.length);
+  return <div>BlogList</div>;
+}
---
+++ components/PreviewSuspense.tsx
@@ -0,0 +1,4 @@
+"use client";
+
+// Once rollup supposts 'use client' module directives then 'next-sanity' will include them & this re-export will no longer be necessary.
+export { PreviewSuspense as default } from "next-sanity/preview";
