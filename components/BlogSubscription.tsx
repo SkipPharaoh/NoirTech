54e87eecf257f6e184c56e063a2cad954548ec8a
skippharaoh <caniggiathompson@gmail.com>
2023-01-27 23:34:02 -0500

    Added AboutSection to the Footer

--- components/BlogSubscription.tsx
+++ components/BlogSubscription.tsx
@@ -2,11 +2,8 @@ import React from "react";
 
 export default function BlogSubscription() {
   return (
-    <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-      <h1
-        className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
-         md:w-2/5 text-center"
-      >
+    <div className="sm:px-12 px-4 bg-[#ffffff19] py-7">
+      <h1 className="text-3xl mb-4 font-semibold text-center">
         <span className="text-teal-400">Free</span> until you're ready to launch
       </h1>
       <div>
@@ -14,11 +11,11 @@ export default function BlogSubscription() {
           type="text"
           placeholder="Enter Your Email"
           className="text-gray-800 text-center
-            w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none border-2 border-black md:w-auto"
+            w-full mr-1 mb-4 py-2.5 rounded px-2 focus:outline-none border-2 border-black"
         />
         <button
-          className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
-           rounded-md text-white md:w-auto w-full"
+          className="bg-teal-400 hover:bg-teal-500 duration-300 py-2.5 font-[Poppins]
+           rounded-md text-white w-full"
         >
           Subscribe
         </button>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,16 @@ import FooterItemsContainer from "./FooterItemsContainer";
 import SocialIcons from "./SocialIcons";
 import { Icons } from "../lib/FooterMenus";
 import BlogSubscription from "./BlogSubscription";
+import AboutSection from "./AboutSection";
 
 export default function Footer() {
   const date = new Date().getFullYear();
   return (
     <footer className="bg-white text-black">
-      <BlogSubscription />
+      <div className="flex justify-around px-6">
+        <AboutSection />
+        <BlogSubscription />
+      </div>
       <FooterItemsContainer />
       <div className="flex justify-center py-2 text-black text-sm">
         {/* TODO: Add company name to copyright below */}
