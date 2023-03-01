c14ff1bdbb81c49943de0bb11f0255f90b80792b
skippharaoh <caniggiathompson@gmail.com>
2023-03-01 10:13:47 -0500

    Fixed tooltip bug

--- components/core/ToolTip.tsx
+++ components/core/ToolTip.tsx
@@ -10,9 +10,9 @@ const Tooltip: React.FC<TooltipProps> = memo(
   ({ children, text, show }: TooltipProps) => {
     const showClass = show ? "opacity-100" : "opacity-0";
     return (
-      <span className="group relative">
+      <span className="relative">
         <span
-          className={`pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white opacity-0 transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-black before:content-[''] ${showClass}`}
+          className={`pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-black before:content-[''] ${showClass}`}
         >
           {text}
         </span>
